24d2f9f47c4aea15eba57ccdcb76f5070d941ca2
kairung <109464720+kairung@users.noreply.github.com>
2023-04-08 13:52:10 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,5 +8,15 @@ import plotly.graph_objects as go
 
 #st.header("kairung")
 st.image("./pic/npru.png")
-st.image("./pic/kairung.jpg")
+html_8 = """
+<div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
+
+
+
+#st.image("./pic/kairung.jpg")
 
