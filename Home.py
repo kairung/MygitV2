cba4b68b69fbc20fec84e0b40c9eb257709cc468
kairung <109464720+kairung@users.noreply.github.com>
2023-04-08 14:08:04 +0700

    .commit

--- Home.py
+++ Home.py
@@ -20,6 +20,20 @@ dt = pd.read_csv("./data/iris.csv")
 
 st.write(dt.head(10))
 
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
+
+dx = [dt1, dt2, dt3, dt4]
+dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+   st.write(dt.head(10))
+   st.bar_chart(dx2)
+   st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
+
 
 
 #st.image("./pic/kairung.jpg")
