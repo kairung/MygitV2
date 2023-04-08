9411a8d672e9ef9722375da356c69485258a5669
kairung <109464720+kairung@users.noreply.github.com>
2023-04-08 14:56:11 +0700

    .commit

--- Home.py
+++ Home.py
@@ -8,7 +8,21 @@ import plotly.graph_objects as go
 
 #st.header("kairung")
 st.image("./pic/npru.png")
+col1, col2, col3 = st.columns(3)
 
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
+
+   
 html_8 = """
 <div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>การทำนายข้อมูลดอกไม้</h5></center>
